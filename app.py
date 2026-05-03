982bafa3d5c8c1e8a7336e7ec75f120fbcdaaa84
indrajeet120 <babuyadavmajhawa@gmail.com>
2026-05-03 17:58:50 +0530

    finish

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from src.screen.home_screen import home_screen
 from src.screen.teacher_screen import teacher_screen
 from src.screen.student_screen import student_screen
-
+from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
     if 'login_type' not in st.session_state:
@@ -17,4 +17,14 @@ def main():
             student_screen()
         case None:
             home_screen()
+
+
+    join_code = st.query_params.get('join-code')
+    if join_code:
+        if st.session_state.login_type != 'student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
+
 main()
